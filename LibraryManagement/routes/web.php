8cfe5858aa81243bbbf019352806af6ae1e3739c
DerickNorlan <dnorlan01@student.ciputra.ac.id>
2024-11-15 22:16:57 +0800

    p

--- LibraryManagement/routes/web.php
+++ LibraryManagement/routes/web.php
@@ -1,32 +1,40 @@
 <?php
-
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\JournalController;
+use App\Http\Controllers\CdController;
+use App\Http\Controllers\NewspaperController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', [DashboardController::class, "index"])->middleware(['auth', 'App\Http\Middleware\LevelCheck:admin,librarian'])->name('dashboard');
 
-// Route resource for books
-Route::resource('/books', \App\Http\Controllers\BookController::class);
-Route::resource('/journals', \App\Http\Controllers\JournalController::class);
-Route::resource('/cds', \App\Http\Controllers\CdController::class);
-Route::resource('/newspapers', \App\Http\Controllers\NewspaperController::class);
-
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'App\Http\Middleware\LevelCheck:admin,librarian'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/books/{sort?}', [BookController::class, 'index'])->name('books');
+    Route::get('/journals/{sort?}', [JournalController::class, 'index'])->name('journals');
+    Route::get('/cds/{sort?}', [CdController::class, 'index'])->name('cds');
+    Route::get('/newspapers/{sort?}', [NewspaperController::class, 'index'])->name('newspapers');
 });
 
-require __DIR__.'/auth.php';
+Route::middleware(['auth', 'App\Http\Middleware\LevelCheck:admin'])->group(function () {
+    Route::get('/librarians', function () {
+        return view('admin.userPage');
+    });
+});
+
+Route::middleware(['auth', 'App\Http\Middleware\LevelCheck:librarian'])->group(function () {
+    Route::get('/display', function () {
+        redirect('books');
+    })->name('display');
+});
+
+
+require __DIR__ . '/auth.php';
